e6a337c70bb3fd799f3c06b3332b52a9b2bbccef
Tim Dolan <timmydolan14@gmail.com>
2023-05-30 16:07:04 -0400

    Server Running

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DB NAME HERE';
-
-connect(connectionString);
+connect('mongodb://127.0.0.1:27017/developersApplications');
 
 module.exports = connection;
